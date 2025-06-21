d4a7ed6206073ff6c22be06616c03d538b18d980
Huy Nguyen <ngquochuy17.tt@gmail.com>
2025-06-21 19:17:02 +0700

    config vercel

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Chào mừng API Couple Quest");
+});
+
 app.use("/api/v1", routes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+module.exports = app;
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
